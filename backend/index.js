af7a7f95024cfc0095d755309f38c60acb27e81f
Dirk <dvanderenden@gmail.com>
2022-11-15 15:34:23 +0100

    jest json changed

--- backend/index.js
+++ backend/index.js
@@ -23,11 +23,11 @@ app.use(cors());
 
 routes(app);
 
-app.get("/", (req, res) => res.send(`CalisthenicsHealth is running on port ${PORT}`));
+app.get("/", (req, res) => res.send(`CalisthenicsHealth is running ${PORT}`));
 
-var server = app.listen(PORT, () => {
-  console.log(`Listening on port: ${PORT}`);
-});
+if (process.env.NODE_ENV !== "test") {
+  app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+}
 // app.listen(PORT, () =>
 //   console.log(`CalisthenicsHealth server is running on port ${PORT}`)
 // );
